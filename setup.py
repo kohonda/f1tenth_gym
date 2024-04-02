9c4e93a94cc062958d1f834ab30101a4c4e260f9
kohonda <0905honda@gmail.com>
2024-04-02 10:58:16 -0700

    wip

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="f1tenth_gym",
+    version="0.1.0",
+    packages=find_packages(where="f1tenth_gym"),
+    package_dir={"": "f1tenth_gym"},
+)
